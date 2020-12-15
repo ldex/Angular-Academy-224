5ebc5264f126229b412879b2653c4bc07216cbd6
ldex <laurent.duveau+git@gmail.com>
2020-12-15 15:20:31 -0500

    End of Day 2

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
 @Component({
@@ -10,9 +13,39 @@ export class ProductDetailComponent implements OnInit {
 
   @Input() product: Product;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router
+  ) { }
+
+  delete() {
+    if(window.confirm('Are you sure ??')) {
+      this
+      .productService
+      .deleteProduct(this.product.id)
+      .subscribe(
+        () => {
+          console.log('Product deleted!' + this.product.name);
+          this.productService.initProducts();
+          this.router.navigateByUrl('/products');
+        },
+        error => console.error(error)
+      )
+    }
+  }
 
   ngOnInit(): void {
+    let id = Number(this.activatedRoute.snapshot.params['id']);
+    this
+      .productService
+      .products$
+      .pipe(
+        map(products => products.find(product => product.id === id))
+      )
+      .subscribe(
+        result => this.product = result
+      )
   }
 
 }
